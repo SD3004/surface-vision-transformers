af4e4255415bdd6080d7b84aea05ddde02c7ab1b
Simon Dahan <simon.dahan@kcl.ac.uk>
2023-05-17 11:41:52 +0100

    update sit

--- models/sit.py
+++ models/sit.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-# @Author: Simon Dahan
-# @Last Modified time: 2021-12-16 10:41:47
+# @Author: Your name
+# @Date:   1970-01-01 01:00:00
+# @Last Modified by:   Your name
+# @Last Modified time: 2022-02-22 11:35:57
 #
 # Created on Fri Oct 01 2021
 #
@@ -10,24 +12,24 @@
 #
 
 '''
-This file contains the implementation of the ViT model: https://arxiv.org/abs/2010.11929 adapted to the case of surface patching. 
-Input data is a sequence of non-overlapping patches. 
+This file contains our implementation of the ViT model: https://arxiv.org/abs/2010.11929
 '''
 
 import torch
 from torch import nn
 
-from einops import repeat
+from einops import rearrange, repeat
 from einops.layers.torch import Rearrange
 
 from vit_pytorch.vit import Transformer, Attention, FeedForward, PreNorm
+
+from timm.models.layers import trunc_normal_
 
 class SiT(nn.Module):
     def __init__(self, *,
                         dim, 
                         depth,
                         heads,
-                        mlp_dim,
                         pool = 'cls', 
                         num_patches = 20,
                         num_classes= 1,
@@ -35,7 +37,15 @@ class SiT(nn.Module):
                         num_vertices = 2145,
                         dim_head = 64,
                         dropout = 0.,
-                        emb_dropout = 0.
+                        emb_dropout = 0.,
+                        bottleneck_dropout = 0.,
+                        mlp_ratio = 4,
+                        use_pe = True,
+                        use_confounds = False,
+                        use_bottleneck= False, 
+                        weights_init = False, 
+                        use_class_token = True,
+                        trainable_pos_emb = True, 
                         ):
 
         super().__init__()
@@ -50,11 +60,29 @@ class SiT(nn.Module):
             nn.Linear(patch_dim, dim),
         )
 
-        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
-        self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
+        if use_bottleneck:
+            self.to_patch_embedding = nn.Sequential(
+            Rearrange('b c n v  -> b n (v c)'),
+            nn.Dropout(bottleneck_dropout),
+            nn.Linear(patch_dim,1024),
+            nn.Dropout(bottleneck_dropout),
+            nn.Linear(1024, dim),
+        )
+
+        if use_bottleneck:
+            print('using bottleneck')
+
+        if use_confounds:
+            self.proj_confound = nn.Sequential(
+                                    nn.BatchNorm1d(1),
+                                    nn.Linear(1,dim))
+        
         self.dropout = nn.Dropout(emb_dropout)
 
-        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
+        self.use_pe = use_pe
+        self.use_confounds = use_confounds
+
+        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_ratio*dim, dropout)
 
         self.pool = pool
         self.to_latent = nn.Identity()
@@ -63,14 +91,46 @@ class SiT(nn.Module):
             nn.Linear(dim, num_classes)
         )
 
-    def forward(self, img):
+        if weights_init: # apply the same weight init as timm's github
+            print('Using initialisation from Timms repo')
+            self.cls_token = nn.Parameter(torch.zeros(1, 1, dim)) if use_class_token else None
+            self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim, requires_grad=trainable_pos_emb) * .02) if use_pe else None
+            self.init_weights()
+
+        else: # apply to weight init from vit_pytorch
+            self.cls_token = nn.Parameter(torch.randn(1, 1, dim)) if use_class_token else None
+            self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim), requires_grad=trainable_pos_emb) if use_pe else None
+
+    def init_weights(self,):
+
+        trunc_normal_(self.pos_embedding, std=.02)
+        if self.cls_token is not None:
+            nn.init.normal_(self.cls_token, std=1e-6) 
+
+        # initialize nn.Linear
+        self.apply(self._init_weights)
+    
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+
+
+    def forward(self, img, confounds=None):
         x = self.to_patch_embedding(img)
+
         b, n, _ = x.shape
 
-        cls_tokens = repeat(self.cls_token, '() n d -> b n d', b = b)
+        if self.use_confounds and (confounds is not None):
+            confounds = self.proj_confound(confounds.view(-1,1))
+            confounds = repeat(confounds, 'b d -> b n d', n=n+1)
+
+        cls_tokens = repeat(self.cls_token, '1 1 d -> b 1 d', b = b)
         
         x = torch.cat((cls_tokens, x), dim=1)
-        x += self.pos_embedding[:, :(n + 1)]
+        if self.use_pe: 
+            x += self.pos_embedding[:, :(n + 1)]
+        if self.use_confounds and (confounds is not None):
+            x += confounds
         x = self.dropout(x)
 
         x = self.transformer(x)
@@ -80,3 +140,11 @@ class SiT(nn.Module):
         x = self.to_latent(x)
 
         return self.mlp_head(x)
+
+if __name__ == '__main__':
+
+    model = SiT(dim = 192,
+                depth = 12,
+                heads= 3,
+                weights_init=True)
+
